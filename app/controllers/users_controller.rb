4fece1557e9c32a3caa9e1d7b847cbd713db1529
César Bouttier <bouttiercesar@gmail.com>
2021-06-01 15:38:13 +0100

    users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,27 +8,4 @@ class UsersController < ApplicationController
     @experience = Experience.new
     @booking = Booking.new(user: @user)
   end
-
-  def new
-    @user = user.new
-  end
-
-  def create
-    @user = User.new(user_params)
-    if @user.save
-      redirect_to experience_path(@experience)
-    else
-      render :new
-    end
-  end
-
-  private
-
-  def set_experience
-    @experience = Experience.find(params[:id])
-  end
-
-  def user_params
-    users.require(:user)
-  end
 end
